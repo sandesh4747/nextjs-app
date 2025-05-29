13e330d23921faf7bece046059c526627445ea5e
sandesh4747 <sangitthapa328@gmail.com>
2025-05-29 20:17:33 +0545

    changes

---
+++ app/_components/Client.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Client() {
+  return (
+    <div>
+      <h1>hello World component</h1>
+    </div>
+  );
+}
--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -1,8 +1,10 @@
+import { redirect } from "next/navigation";
 import React from "react";
 
 export default async function Page({ params }) {
   const { id } = await params;
-  console.log(id);
+  // if (id === "100") redirect("/product");
+  // console.log(id);
   return (
     <div>
       <h1>This is product Detail Page</h1>
